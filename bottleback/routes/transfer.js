d66b2c8fede9b5d6ea00c990038ee9c65831991f
abdelfattah10 <abdelfattah10@yahoo.com>
2019-01-23 01:48:30 +0200

    first commit

--- bottleback/routes/transfer.js
+++ bottleback/routes/transfer.js
@@ -36,8 +36,6 @@ async function transfer(from, to, amount) {
         }
 
     if (A.points < 0) {
-      // If A would have negative balance, fail and abort the transaction
-      // `session.abortTransaction()` will undo the above `findOneAndUpdate()`
       throw new Error('Insufficient points');
     }
 
@@ -51,34 +49,10 @@ async function transfer(from, to, amount) {
     console.log({ from: A, to: B });
     return { from: A, to: B };
   } catch (error) {
-    // If an error occurred, abort the whole transaction and
-    // undo any changes that might have happened
     await session.abortTransaction();
     session.endSession();
-    throw error; // Rethrow so calling function sees error
+    throw error;
   }
 }
-
-// router.post('/transfer', requireAuth, function(req, res){
-//   const destemail = req.body.destemail;
-//   const amount = req.body.amount
-//   const userPoints = req.user.points
-//
-//    if(userPoints < amount){
-//      res.status(400).json({error: 'Do not have enough points'})
-//    } else {
-//
-//      User.findOneAndUpdate({email: req.user.email}, { $inc: { points: -amount }}, {new: true}, function(err, result){
-//        if(err)
-//          return err
-//
-//          User.findOneAndUpdate({email: destemail}, { $inc: { points: amount }}, {new: true}, function(err, result){
-//            if(err)
-//              return err
-//              res.json({message: 'Transfer is done successfully'})
-//          })
-//      })
-//   }
-// })
 
 module.exports = router;
